22266ab7dcd15097a898fc2a6c0258c7fbb8efaf
Andre-Gross <andre.handy94@gmail.com>
2025-12-21 06:45:36 +0100

    chore: rename navigation-component to navigation.component and move to shared folder

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   imports: [],
-  templateUrl: './navigation-component.html',
-  styleUrl: './navigation-component.scss',
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
 
