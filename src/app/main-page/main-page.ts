84ebcae618d1ed98dac8e6d6350c4687d12e8a98
Andre-Gross <andre.handy94@gmail.com>
2026-01-12 17:26:15 +0100

    feat(section-title): add SectionTitleComponent with initial styling and example title

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { LandingPage } from "./landing-page/landing-page";
 import { SwitchSectionArea } from "../shared/switch-section-area/switch-section-area";
+import { SectionTitleComponent } from './section-title/section-title.component';
 
 @Component({
   selector: 'app-main-page',
   imports: [
     LandingPage,
-    SwitchSectionArea
+    SwitchSectionArea,
+SectionTitleComponent
   ],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
