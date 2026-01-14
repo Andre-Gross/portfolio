ca6d3299520532d418d84d609e9280b6375d87d4
Andre-Gross <andre.handy94@gmail.com>
2026-01-14 17:53:54 +0100

    feat: implement horizontal scroll with mouse wheel

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -4,15 +4,26 @@ import { SwitchSectionArea } from "../shared/switch-section-area/switch-section-
 import { SectionTitleComponent } from './section-title/section-title.component';
 
 @Component({
-  selector: 'app-main-page',
-  imports: [
-    LandingPage,
-    SwitchSectionArea,
-SectionTitleComponent
-  ],
-  templateUrl: './main-page.html',
-  styleUrl: './main-page.scss',
+    selector: 'app-main-page',
+    imports: [
+        LandingPage,
+        SwitchSectionArea,
+        SectionTitleComponent,
+    ],
+    templateUrl: './main-page.html',
+    styleUrl: './main-page.scss',
 })
 export class MainPage {
+
+
+    onWheel(event: WheelEvent): void {
+        if (event.deltaY !== 0) {
+            const element = event.currentTarget as HTMLElement;
+
+            event.preventDefault();
+
+            element.scrollLeft += 12 * event.deltaY;
+        }
+    }
 
 }
