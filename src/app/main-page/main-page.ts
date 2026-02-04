7e69fe32c47390229832307950784561d55e11c4
Andre-Gross <andre.handy94@gmail.com>
2026-02-04 17:56:42 +0100

    feat: start adding correct content to Why Me section

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -20,6 +20,29 @@ export class MainPage {
 
     currentLang: 'en' | 'de' = 'en';
 
+    mainPageData = {
+        en: {
+            whyMe: {
+                title: 'Why me',
+                abouteMe: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor.',
+                iAm: 'I am',
+                location: 'located in Berlin, Germany',
+                remote: 'open to work remotly',
+                relocate: 'open to relocate',
+            }
+        },
+        de: {
+            whyMe: {
+                title: 'Warum mich',
+                abouteMe: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor.',
+                iAm: 'Ich bin',
+                location: 'Ich wohne in Berlin',
+                remote: 'Ich bin offen für Home Office',
+                relocate: 'Ich bin bereit für einen Umzug',
+            }
+        }
+    }
+
     
     constructor() {
         if (navigator.language.startsWith('de')) {
--- src/app/main-page/why-me/why-me.component.ts
+++ src/app/main-page/why-me/why-me.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SectionTitleComponent } from '../../shared/section-title/section-title.component';
 
 
 @Component({
     selector: 'app-why-me',
-    imports: [],
+    imports: [SectionTitleComponent],
     templateUrl: './why-me.component.html',
     styleUrl: './why-me.component.scss',
 })
 export class WhyMeSection {
+    @Input() data!: any;
 
 }
