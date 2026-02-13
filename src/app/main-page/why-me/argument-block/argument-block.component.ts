438cc0514c942c7db79a9a12771b44fbdf7bdc26
Andre-Gross <andre.handy94@gmail.com>
2026-02-13 16:28:36 +0100

    refactor(why-me): remove unused ngStyle import from argument block

--- src/app/main-page/why-me/argument-block/argument-block.component.ts
+++ src/app/main-page/why-me/argument-block/argument-block.component.ts
@@ -1,13 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { SingleArgument } from './single-argument/single-argument.component';
-import { KeyValuePipe, NgStyle } from '@angular/common';
+import { KeyValuePipe} from '@angular/common';
 
 @Component({
   selector: 'app-argument-block',
   imports: [
     SingleArgument,
-    KeyValuePipe,
-    NgStyle
+    KeyValuePipe
 ],
   templateUrl: './argument-block.component.html',
   styleUrl: './argument-block.component.scss',
