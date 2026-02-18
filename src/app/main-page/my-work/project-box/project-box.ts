60a172baa3d1c7933f7394b2b4842d98b7d706e4
Andre-Gross <andre.handy94@gmail.com>
2026-02-18 17:40:53 +0100

    refactor(my-work): restructure project detail sections and image wrapper

--- src/app/main-page/my-work/project-box/project-box.ts
+++ src/app/main-page/my-work/project-box/project-box.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeadlineComponent } from './headline/headline.component';
-import { UpperSectionComponent } from './upper-section/upper-section.component';
 
 @Component({
   selector: 'app-project-box',
   imports: [
     HeadlineComponent,
-    UpperSectionComponent
   ],
   templateUrl: './project-box.html',
   styleUrl: './project-box.scss',
 })
 export class ProjectBoxComponent {
+
+    @Input() project!: any;
 
 }
