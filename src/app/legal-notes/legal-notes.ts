e5b9d82fb524de018802b6c8946e3ad10f49e915
Andre-Gross <andre.handy94@gmail.com>
2026-04-23 09:31:04 +0200

    fix(legal-notes): ensure horizontal scrolling and prevent content squeezing

--- src/app/legal-notes/legal-notes.ts
+++ src/app/legal-notes/legal-notes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ContentService } from '../shared/services/content/content.service';
+import { ScrollService } from '../shared/services/scroll/scroll.service';
 
 @Component({
   selector: 'app-legal-notes',
@@ -9,5 +10,16 @@ import { ContentService } from '../shared/services/content/content.service';
   styleUrl: './legal-notes.scss'
 })
 export class LegalNotes {
-  constructor(public contentService: ContentService) {}
+
+
+  constructor(private scrollService: ScrollService, public contentService: ContentService) { }
+
+
+  onWheel(event: WheelEvent): void {
+    if (event.deltaY !== 0) {
+      const element = event.currentTarget as HTMLElement;
+      event.preventDefault();
+      element.scrollLeft += 12 * event.deltaY;
+    }
+  }
 }
