bd7f3c189781beefa94fe11e0227908940f82c9c
Andre-Gross <andre.handy94@gmail.com>
2026-04-27 09:40:48 +0200

    feat(navigation): redirect to main page on icon click

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ScrollService } from '../../services/scroll/scroll.service';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
 
-  
+
   constructor(private scrollService: ScrollService) { }
 
 
