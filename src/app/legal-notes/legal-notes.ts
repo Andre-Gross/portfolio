2a3ec260474dc0f4d75bade41a1546d411eccb6e
Andre-Gross <andre.handy94@gmail.com>
2026-04-27 10:42:15 +0200

    feat(legal-notes): enable internal section navigation via section titles and arrows

--- src/app/legal-notes/legal-notes.ts
+++ src/app/legal-notes/legal-notes.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 
 import { ContentService } from '../shared/services/content/content.service';
 import { ScrollService } from '../shared/services/scroll/scroll.service';
@@ -6,6 +6,7 @@ import { ScrollService } from '../shared/services/scroll/scroll.service';
 import { TopBarComponent } from '../shared/components/top-bar/top-bar.component';
 import { SwitchArrowComponent } from '../shared/components/switch-arrow/switch-arrow.component';
 import { EndBlock } from '../shared/components/end-block/end-block';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-legal-notes',
@@ -20,8 +21,36 @@ import { EndBlock } from '../shared/components/end-block/end-block';
 })
 export class LegalNotes {
 
+  readonly start = viewChild('start', { read: ElementRef });
+  readonly secondColumn = viewChild('secondColumn', { read: ElementRef });
+  private scrollSub!: Subscription;
+
 
   constructor(private scrollService: ScrollService, public contentService: ContentService) { }
+
+
+  ngOnInit() {
+    this.scrollSub = this.scrollService.scrollRequest$.subscribe(sectionId => {
+      const sectionMap: Record<string, ElementRef | undefined> = {
+        'start': this.start(),
+        'secondColumn': this.secondColumn(),
+
+      };
+
+      const target = sectionMap[sectionId];
+
+      if (target) {
+        this.scrollToElement(target);
+      } else {
+        console.warn(`Sektion mit der ID "${sectionId}" wurde nicht gefunden.`);
+      }
+    });
+  }
+
+
+  ngOnDestroy() {
+    this.scrollSub?.unsubscribe();
+  }
 
 
   onWheel(event: WheelEvent): void {
@@ -29,6 +58,30 @@ export class LegalNotes {
       const element = event.currentTarget as HTMLElement;
       event.preventDefault();
       element.scrollLeft += 12 * event.deltaY;
+    }
+  }
+
+
+  scrollToElement(element: any) {
+    const target = element instanceof ElementRef ? element.nativeElement : element.nativeElement;
+
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    });
+  }
+
+
+  triggerScroll(sectionId: string): void {
+    const sectionMap: Record<string, ElementRef | undefined> = {
+      'start': this.start(),
+      'secondColumn': this.secondColumn(),
+    };
+
+    const target = sectionMap[sectionId];
+    if (target) {
+      this.scrollToElement(target);
     }
   }
 }
