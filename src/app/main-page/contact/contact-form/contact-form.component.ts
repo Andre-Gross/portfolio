e2350d7790148caabd662b04b5a3452375394ef4
Andre-Gross <andre.handy94@gmail.com>
2026-05-03 18:32:12 +0200

    feat(contact-form): implement email submission via PHP backend
    - Note: Functionality requires server-side environment for mail() support.

--- src/app/main-page/contact/contact-form/contact-form.component.ts
+++ src/app/main-page/contact/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { FormsModule, NgForm } from '@angular/forms';
 
 import { ContentService } from '../../../shared/services/content/content.service';
@@ -18,7 +19,8 @@ import { ContentService } from '../../../shared/services/content/content.service
 
 export class ContactFormComponent {
 
-  constructor(public contentService: ContentService) { }
+  http = inject(HttpClient);
+  mailTest = true;
 
   contactData = {
     name: '',
@@ -27,19 +29,37 @@ export class ContactFormComponent {
     privacyAccepted: false
   };
 
+  post = {
+    endPoint: 'https://andre-gross.dev/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+
+  constructor(public contentService: ContentService) { }
+
+
   onSubmit(contactForm: NgForm) {
-    if (contactForm.valid) {
-      console.log('Formular-Inhalt:', this.contactData);
+    if (contactForm.submitted && contactForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
 
+            contactForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (contactForm.submitted && contactForm.form.valid && this.mailTest) {
+      console.log("Mailtest sucessfully")
       contactForm.resetForm();
-
-      this.contactData = {
-        name: '',
-        email: '',
-        message: '',
-        privacyAccepted: false
-      };
     }
   }
 }
-
