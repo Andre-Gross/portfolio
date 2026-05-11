cb4e44100ed9f78e39ae58ce5addb4238a7dd1b5
Andre-Gross <andre.handy94@gmail.com>
2026-05-11 10:55:57 +0200

    feat(nav): implement responsive navigation for mobile viewports

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
 import { ScrollService } from '../../services/scroll/scroll.service';
+import { ContentService } from '../../services/content/content.service';
+
+import { LangSwitchComponent } from '../lang-switch/lang-switch.component';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [
+    RouterLink,
+    LangSwitchComponent,
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
 
+  isMenuOpen = signal(false);
+  private scrollService = inject(ScrollService);
+  public contentService = inject(ContentService);
 
-  constructor(private scrollService: ScrollService) { }
+
+  setMenu(shallOpen?: boolean): void {
+    if (shallOpen !== undefined) {
+      this.isMenuOpen.set(shallOpen);
+    } else {
+      this.isMenuOpen.update(state => !state);
+    }
+  }
 
 
   triggerScroll(sectionId: string): void {
     this.scrollService.requestScroll(sectionId);
+    this.setMenu(false);
   }
 }
