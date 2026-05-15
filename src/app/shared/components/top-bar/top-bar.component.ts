2c6ffcac401d5c541918b1979e66d0b1957f5989
Andre-Gross <andre.handy94@gmail.com>
2026-05-15 19:50:40 +0200

    feat(contact-links): extract contact links into separate component

--- src/app/shared/components/top-bar/top-bar.component.ts
+++ src/app/shared/components/top-bar/top-bar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { LangSwitchComponent } from '../lang-switch/lang-switch.component';
+import { ContactLinksComponent } from '../contact-links/contact-links.component';
 
 @Component({
     selector: 'app-top-bar',
-    imports: [LangSwitchComponent],
+    imports: [
+        ContactLinksComponent,
+        LangSwitchComponent
+    ],
     templateUrl: './top-bar.component.html',
     styleUrl: './top-bar.component.scss',
 })
